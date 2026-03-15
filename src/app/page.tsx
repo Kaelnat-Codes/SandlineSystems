f32bbd2804f9798b9afc1690b37dfddf290a0de4
Abhi Bagai <abhi.bagai@gmail.com>
2026-03-15 16:35:59 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,6 @@ import OfficeSurveyEmbed from "@/components/OfficeSurveyEmbed";
 export default function Survey() {
   return (
     <main style={{ padding: "2rem" }}>
-      <div>
-        <h1>
-          correct build test march 15th
-        </h1>
-      </div>
       <OfficeSurveyEmbed
         src="https://forms.office.com/Pages/ResponsePage.aspx?id=cSrPUOv0sk-FMbg5sbe_fLlSHVjUENxEtMUMZADdakNUQURBTU1CODM4QzlSMlQ0UExBWDdQVExWViQlQCN0PWcu&embed=true"
         title="Customer Survey"
